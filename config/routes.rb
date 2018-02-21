c6b403c6771e232cfef5c62e91ef7188aa2aabbc
YukiMizutani <mizutaniyuki3@gmail.com>
2018-02-21 15:53:27 +0000

    follow, unfollow

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,13 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
-    get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :new, :create]
-  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create] do
+    member do
+      get :followings
+      get :followers
+    end
+  end
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 end
